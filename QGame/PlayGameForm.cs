c88e0e76b55a4892de8e662c5728ceede6638fbb
Nathan Hannon <nathan@hannon.me>
2018-10-09 10:32:17 -0400

    Added Load file functionality

--- QGame/PlayGameForm.cs
+++ QGame/PlayGameForm.cs
@@ -19,13 +19,78 @@ namespace QGame
         {
             InitializeComponent();
         }
-        
+
+        enum SquareType
+        {
+            Blank,
+            BrickWall,
+            RedSquare,
+            BlueSquare,
+            GreenSquare,
+            YellowSquare,
+            RedExit,
+            BlueExit,
+            GreenExit,
+            YellowExit
+        }
+
+        private SquareType _squaretype = SquareType.Blank;
+
         private void DoLoad(string fileName)
         {
             StreamReader loadLevel = new StreamReader(fileName);
-            //TODO: Add logic for loading from text file
-        }
 
+            for (int i = 0; i < LevelDesignForm._rowSqaures; i++)
+            {
+                for (int j = 0; j < LevelDesignForm._columnSquares; j++)
+                {
+                    String line;
+                    while ((line = loadLevel.ReadLine()) != null)
+                    {
+                        if (LevelDesignForm._square[i, j].Image == Properties.Resources.blank_square)
+                        {
+                            Controls.Remove(LevelDesignForm._square[i, j]);
+                        }
+                        else if (LevelDesignForm._square[i, j].Image == Properties.Resources.brick_wall)
+                        {
+                            loadLevel.ReadLine();
+                        }
+                        else if (LevelDesignForm._square[i, j].Image == Properties.Resources.red_square)
+                        {
+                            loadLevel.ReadLine();
+                        }
+                        else if (LevelDesignForm._square[i, j].Image == Properties.Resources.blue_square)
+                        {
+                            loadLevel.ReadLine();
+                        }
+                        else if (LevelDesignForm._square[i, j].Image == Properties.Resources.green_square)
+                        {
+                            loadLevel.ReadLine();
+                        }
+                        else if (LevelDesignForm._square[i, j].Image == Properties.Resources.yellow_square)
+                        {
+                            loadLevel.ReadLine();
+                        }
+                        else if (LevelDesignForm._square[i, j].Image == Properties.Resources.exit_red)
+                        {
+                            loadLevel.ReadLine();
+                        }
+                        else if (LevelDesignForm._square[i, j].Image == Properties.Resources.exit_blue)
+                        {
+                            loadLevel.ReadLine();
+                        }
+                        else if (LevelDesignForm._square[i, j].Image == Properties.Resources.exit_green)
+                        {
+                            loadLevel.ReadLine();
+                        }
+                        else if (LevelDesignForm._square[i, j].Image == Properties.Resources.exit_yellow)
+                        {
+                            loadLevel.ReadLine();
+                        }
+                    }
+                }
+            }
+        }
         private void loadToolStripMenuItem_Click(object sender, EventArgs e)
         {
             DialogResult result = openFile.ShowDialog();
